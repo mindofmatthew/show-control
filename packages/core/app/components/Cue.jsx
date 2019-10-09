05a9961d0a8d38e695c3b53d90db207939970d78
Matthew Kaney <matthew@mindofmatthew.com>
2019-10-08 22:39:10 -0400

    Add styling and icons

--- packages/core/app/components/Cue.jsx
+++ packages/core/app/components/Cue.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+
 import { ACTION_TYPE } from '../reducers/symbols';
 
 export function Cue({ dispatch, cue: { id, name, data } }) {
   return (
     <li className="cue">
       <div className="controls">
+        <button>
+          <FontAwesomeIcon icon={faEdit} />
+        </button>
         <button
           onClick={() => {
             dispatch({ [ACTION_TYPE]: 'DELETE_CUE', id });
           }}>
-          Delete
+          <FontAwesomeIcon icon={faTrash} />
         </button>
       </div>
       {id}
