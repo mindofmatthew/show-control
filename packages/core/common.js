675eb1c1c74f951da5fbe568b591a76b2a30a4f3
Caleb Eggensperger <calebegg@google.com>
2019-10-12 23:29:47 -0400

    Make IDs actually 6 characters long

--- packages/core/common.js
+++ packages/core/common.js
@@ -1,3 +1,7 @@
+const LENGTH = 6;
+
 exports.genId = () => {
-  return Math.floor(Math.random() * 16 ** 6).toString(16);
+  return Math.floor(Math.random() * 16 ** LENGTH)
+    .toString(16)
+    .padStart(LENGTH, '0');
 };
