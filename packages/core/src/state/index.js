03f485b37fedaca60838c9c32252cab3a047708e
Matthew Kaney <matthew@mindofmatthew.com>
2019-10-23 21:31:43 -0500

    Deleting an active cue sets the active cue to null

--- packages/core/src/state/index.js
+++ packages/core/src/state/index.js
@@ -12,6 +12,12 @@ exports.mutate = (state, action) =>
       case 'SET_CURRENT_CUE':
         draft.volatile.currentCue = action.id;
         return;
+      case 'DELETE_CUE':
+        if (draft.volatile.currentCue === action.id) {
+          draft.volatile.currentCue = null;
+        }
+        cues.mutate(draft.cues, action);
+        return;
       default:
         config.mutate(draft.config, action);
         cues.mutate(draft.cues, action);
